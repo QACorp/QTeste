95b963974cd0b0cf0c1b2d0cfaf03ed565160521
Marco Aurélio Lima Fernandes <malf88@gmail.com>
2023-03-21 22:49:33 -0300

    Adicionada laravel-data.

---
+++ app/Modules/Example/Controller/ExampleController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Modules\Example\Controller;
+
+use App\Http\Controllers\Controller;
+class ExampleController extends Controller
+{
+
+    public function myMethod() {
+        return 'teste';
+    }
+}
