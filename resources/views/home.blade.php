759d3c3196009d4c5c151a841d649b0a3c2ef218
Marco Aurélio Lima Fernandes <malf88@gmail.com>
2023-06-16 00:44:46 -0300

    Melhorias nas exibições dos cards.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,15 @@
 @stop
 
 @section('content')
-        <div class="d-flex flex-wrap flex-column flex-md-row">
+        <div class="">
+            <div class="row">
             @foreach(\App\System\Config\DashboardConfig::getDashboardWidget() as $key => $widget)
                 <div class="col-md-{{ $widget->getSize() }} col-12">
                    {!! Blade::render($widget->getComponente()) !!}
                 </div>
             @endforeach
-            </div>
         </div>
-    </div>
+        </div>
+
 
 @stop
