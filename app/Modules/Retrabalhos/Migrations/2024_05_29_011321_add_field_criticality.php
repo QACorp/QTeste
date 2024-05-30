d1e05e103bbab2ddceb81b1b75133edff70dcb54
Marco Fernandes <malf88@gmail.com>
2024-05-30 00:14:51 -0300

    Correções pontuais.

--- app/Modules/Retrabalhos/Migrations/2024_05_29_011321_add_field_criticality.php
+++ app/Modules/Retrabalhos/Migrations/2024_05_29_011321_add_field_criticality.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Modules\Retrabalhos\Enums\CriticidadeEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projetos.retrabalhos', function (Blueprint $table) {
-            $table->enum('criticidade',['Crítico', 'Baixa', 'Média', 'Alta'])->nullable();
+            $table->enum('criticidade',['Crítico', 'Baixa', 'Média', 'Alta'])->default(CriticidadeEnum::BAIXA->value);
         });
     }
 
