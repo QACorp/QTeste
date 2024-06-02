bb1ecc17f6483a9286b353b71b0d0835528d02e5
Marco Fernandes <malf88@gmail.com>
2024-06-02 00:33:35 -0300

    Atualização do AdminLTE

--- resources/views/vendor/adminlte/auth/login.blade.php
+++ resources/views/vendor/adminlte/auth/login.blade.php
@@ -18,6 +18,8 @@
     @php( $password_reset_url = $password_reset_url ? url($password_reset_url) : '' )
 @endif
 
+@section('auth_header', __('adminlte::adminlte.login_message'))
+
 @section('auth_body')
     <form action="{{ $login_url }}" method="post">
         @csrf
@@ -60,21 +62,20 @@
 
         {{-- Login field --}}
         <div class="row">
-            <div class="col-12">
+            <div class="col-7">
                 <div class="icheck-primary" title="{{ __('adminlte::adminlte.remember_me_hint') }}">
                     <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                     <label for="remember">
-                        Manter conectado
+                        {{ __('adminlte::adminlte.remember_me') }}
                     </label>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
+
+            <div class="col-5">
                 <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
                     <span class="fas fa-sign-in-alt"></span>
-                    Entrar
+                    {{ __('adminlte::adminlte.sign_in') }}
                 </button>
             </div>
         </div>
