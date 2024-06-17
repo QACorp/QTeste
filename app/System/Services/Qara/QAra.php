13606bcad2328a493fefceb6645b028e1c36b5ab
Marco Fernandes <malf88@gmail.com>
2024-06-16 22:21:06 -0300

    Alteração da biblioteca usada pela QAra.

--- app/System/Services/Qara/QAra.php
+++ app/System/Services/Qara/QAra.php
@@ -3,14 +3,24 @@
 namespace App\System\Services\Qara;
 
 
-use OpenAI\Laravel\Facades\OpenAI;
+use OpenAI;
 use Spatie\LaravelData\DataCollection;
 
 class QAra
 {
     const MODEL = 'gpt-3.5-turbo';
-    public static function getChat(DataCollection $messages){
-        return OpenAI::chat()->create([
+    public function __construct(
+        private readonly string $apiKey
+    ){}
+
+    public static function factory(string $apiKey):QAra
+    {
+        return new self($apiKey);
+    }
+    public function getChat(DataCollection $messages){
+
+        $client = OpenAI::client($this->apiKey);
+        return $client->chat()->create([
             'response_format' => ["type" => "json_object" ],
             'model' => self::MODEL,
             'messages' => $messages->toArray()
