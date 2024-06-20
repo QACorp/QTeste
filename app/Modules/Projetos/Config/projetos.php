f2513acb171832f4e240f5184ca02496c26afd3f
Marco Fernandes <malf88@gmail.com>
2024-06-19 22:16:07 -0300

    Adicionado atributo shift no menu.

--- app/Modules/Projetos/Config/projetos.php
+++ app/Modules/Projetos/Config/projetos.php
@@ -12,13 +12,15 @@ return [
                 'text' => 'Aplicações',
                 'can'   => 'LISTAR_APLICACAO',
                 'active' => ['projetos/aplicacoes/*'],
+                'shift' => 'ml-3',
                 'submenu' => [
                     [
                         'text' => 'Listar aplicações',
                         'route'  => 'aplicacoes.index',
                         'icon'  => 'fas fa-list',
                         'active' => ['projetos/aplicacoes/*'],
-                        'can'   => 'LISTAR_APLICACAO'
+                        'can'   => 'LISTAR_APLICACAO',
+                        'shift' => 'ml-4',
                     ]
                 ]
             ],
@@ -29,12 +31,14 @@ return [
                 'text' => 'Testes',
                 'can'   => ['LISTAR_CASO_TESTE', 'LISTAR_PLANO_TESTE'],
                 'active' => ['projetos/casos-teste/*'],
+                'shift' => 'ml-3',
                 'submenu' => [
                     [
                         'text' => 'Casos de teste',
                         'icon'  => 'fas fa-cubes',
                         'key' => 'casos_teste_index',
                         'active' => ['projetos/casos-teste/*'],
+                        'shift' => 'ml-4',
                         'submenu' => [
                             [
                                 'text' => 'Listar',
@@ -42,6 +46,7 @@ return [
                                 'icon'  => 'fas fa-list',
                                 'active' => ['projetos/casos-teste/','projetos/casos-teste/inserir/*', 'projetos/casos-teste/editar/*'],
                                 'can'   => 'LISTAR_CASO_TESTE',
+                                'shift' => 'ml-5',
                             ],
                         ]
                     ],
@@ -50,7 +55,8 @@ return [
                         'route'  => 'aplicacoes.planos-teste.index',
                         'icon'  => 'fas fa-file-alt',
                         'active' => ['projetos/planos-teste/*'],
-                        'can'   => 'LISTAR_PLANO_TESTE'
+                        'can'   => 'LISTAR_PLANO_TESTE',
+                        'shift' => 'ml-4',
                     ],
 
 
