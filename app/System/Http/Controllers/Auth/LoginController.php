efd83a2479bcd83014ba0609eb194193dfe3a067
Marco Fernandes <malf88@gmail.com>
2024-07-16 23:50:41 -0300

    Validação de usuário ativo.

--- app/System/Http/Controllers/Auth/LoginController.php
+++ app/System/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,11 @@ namespace App\System\Http\Controllers\Auth;
 use App\System\Http\Controllers\Controller;
 use App\System\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class LoginController extends Controller
 {
@@ -28,6 +32,41 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+    public function login(Request $request): JsonResponse|RedirectResponse|Response
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            if($request->hasSession()){
+                $request->session()->put('auth.password_confirmed_at', time());
+                return $this->sendLoginResponse($request);
+            }
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    protected function attemptLogin(Request $request)
+    {
+        return $this->guard()->attempt(
+            [...$this->credentials($request), 'active' => true], $request->boolean('remember')
+        );
+    }
 
 
     /**
