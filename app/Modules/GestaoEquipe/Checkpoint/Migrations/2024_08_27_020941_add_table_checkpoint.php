517067ec37418aec334c2cc28b75c027930291e1
Marco Fernandes <malf88@gmail.com>
2024-08-27 12:58:56 -0300

    Criado migration dos checkpoints

--- app/Modules/GestaoEquipe/Checkpoint/Migrations/2024_08_27_020941_add_table_checkpoint.php
+++ app/Modules/GestaoEquipe/Checkpoint/Migrations/2024_08_27_020941_add_table_checkpoint.php
@@ -4,14 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    const SCHEMA = 'gestao_equipe.';
+
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        //
+        Schema::create(self::SCHEMA . 'checkpoints', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('projeto_id')->nullable()->constrained('projetos.projetos');
+            $table->foreignId('criador_user_id')->constrained('users');
+            $table->foreignId('user_id')->constrained('users');
+            $table->text('descricao');
+            $table->dateTime('data');
+            $table->string('tarefa')->nullable();
+            $table->boolean('compareceu');
+            $table->timestamps();
+            $table->softDeletes();
+        });
     }
 
     /**
